b7a4de2a839d1c5c73cac206f78cdf8abab1b8b4
Nicolas Agudelo Hincapie <nagudelo@paradigmasolutions.com>
2024-03-04 13:15:37 -0500

    added admin server project

---
+++ admin-sevice/src/test/java/com/mslab/admin/AdminSeviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mslab.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminSeviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
